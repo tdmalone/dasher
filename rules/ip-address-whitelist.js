da099323980c2d5a38448813ce21fe52a76fdd96
Tim Malone <tdmalone@gmail.com>
2018-03-01 08:48:50 +1100

    Update ip-address-whitelist rule to use secure config

--- rules/ip-address-whitelist.js
+++ rules/ip-address-whitelist.js
@@ -1,10 +1,10 @@
 
 function ( user, context, callback ) {
 
-  // Authorized IPs.
-  var whitelist = [
-    'ENTER.IP.ADDRESS.HERE'
-  ];
+  // Authorized IPs. Add a JSON array as 'ip_address_whitelist' in your Rules settings.
+  // @example ["123.45.67.89","98.76.54.32"]
+  // @see https://manage.auth0.com/#/rules
+  var whitelist = JSON.parse( configuration.ip_address_whitelist );
 
   var errorMessage = 'Access denied (ip-address-whitelist).';
 
