7a98d9be48455e79a53ece8fec15b803a6223917
Tim Malone <tdmalone@gmail.com>
2018-03-01 09:27:41 +1100

    Now that we can, let's fix linting issues in Auth0 Rules

--- rules/disable-signups.js
+++ rules/disable-signups.js
@@ -1,27 +1,37 @@
 
-function rule( user, context, callback ) {
+/* global auth0, UnauthorizedError */
+
+'use strict';
+
+const SINGLE_LOGIN = 1;
+
+/**
+ * Denies access to any user that doesn't already exist in the system. The user will still be
+ * created, but with a metadata setting that will be checked for if they try to log in again.
+ */
+function rule( user, context, callback ) { // eslint-disable-line no-unused-vars
 
   var errorMessageNew = 'Access denied (disable-signups-new).';
   var errorMessageRetry = 'Access denied (disable-signups-retry).';
 
   // Initialize app_metadata.
-  user.app_metadata = user.app_metadata || {};
+  user.app_metadata = user.app_metadata || {}; // eslint-disable-line camelcase
 
   // Check if the user has already tried to signup previously.
-  if ( user.app_metadata.is_rule_e14_signup ) {
-    return callback( errorMessageRetry );
+  if ( user.app_metadata.isDisabledSignup ) {
+    return callback( new UnauthorizedError( errorMessageRetry ) );
   }
 
   // If it is the first login (hence the `signup`).
-  if ( context.stats.loginsCount === 1 ) {
+  if ( SINGLE_LOGIN === context.stats.loginsCount ) {
 
     // Set a flag in case the user tries again.
-    user.app_metadata.is_rule_e14_signup = true;
+    user.app_metadata.isDisabledSignup = true;
 
     // Store the app_metadata.
     auth0.users.updateAppMetadata( user.user_id, user.app_metadata )
       .then( function() {
-        return callback( errorMessageNew );
+        return callback( new UnauthorizedError( errorMessageNew ) );
       })
       .catch( function( err ) {
         callback( err );
@@ -34,4 +44,4 @@ function rule( user, context, callback ) {
   // Else, we have a pre-existing user.
   callback( null, user, context );
 
-}
+} // Function rule.
--- rules/ip-address-whitelist.js
+++ rules/ip-address-whitelist.js
@@ -1,14 +1,24 @@
 
-function rule( user, context, callback ) {
+/* global configuration, UnauthorizedError */
 
-  // Authorized IPs. Add a JSON array as 'ip_address_whitelist' in your Rules settings.
-  // @example ["123.45.67.89","98.76.54.32"]
-  // @see https://manage.auth0.com/#/rules
+'use strict';
+
+/**
+ * Denies access to IP address not listed in a custom whitelist.
+ *
+ * To use, add a JSON array as 'ip_address_whitelist' in your Rules settings
+ * (https://manage.auth0.com/#/rules). For example, ["123.45.67.89","98.76.54.32"]
+ * If this is not supplied, or is invalid JSON, this rule will fail and return an
+ * error.
+ */
+function rule( user, context, callback ) { // eslint-disable-line no-unused-vars
+
+  // Get and parse authorized IPs from config.
   var whitelist = JSON.parse( configuration.ip_address_whitelist );
 
   var errorMessage = 'Access denied (ip-address-whitelist).';
 
-  var userHasAccess = whitelist.some( function ( ip ) {
+  var userHasAccess = whitelist.some( function( ip ) {
     return context.request.ip === ip;
   });
 
@@ -18,4 +28,4 @@ function rule( user, context, callback ) {
 
   return callback( null, user, context );
 
-}
+} // Function rule.
