05e9f268390df5c176c3d216c0364862a72a1c81
Tim Malone <tdmalone@gmail.com>
2018-03-01 23:09:16 +1100

    Add basic styles + style linting + webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,29 @@
 'use strict';
 
 module.exports = {
-  entry:  './index.js',
+
+  entry: [ './index.js', './src/main.scss' ],
+
   output: {
     filename: 'bundle.js'
+  },
+
+  // @see https://github.com/webpack-contrib/sass-loader#examples
+  module: {
+    rules: [ {
+      test: /\.scss$/,
+      use:  [
+        {
+          loader: 'style-loader' // Creates style nodes from JS strings.
+        },
+        {
+          loader: 'css-loader' // Translates CSS into CommonJS.
+        },
+        {
+          loader: 'sass-loader' // Compiles Sass to CSS.
+        }
+      ]
+    } ]
   }
+
 };
